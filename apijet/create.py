da5e965e68a6ead4d453d233bc493a1ca62e6fc0
Alfonso Farruggia <alfarruggia@gmail.com>
2022-09-08 06:01:43 +0100

    generate project folders

--- apijet/create.py
+++ apijet/create.py
@@ -10,24 +10,33 @@ def create(name: str, port: int, root_dir: str) -> None:
     if check_write_permission(root_dir) is False:
         return False
 
-    folder = f"{root_dir}/{name}"
-    project_file = f"{folder}/{name}.json"
+    main_folder = f"{root_dir}/{name}"
+    project_file = f"{main_folder}/{name}.json"
 
     file_project = {
         'name': name,
         'port': port,
-        'folder': folder,
+        'folder': main_folder,
         'project_file': project_file
     }
 
     # check if folder exists
-    if Path(folder).is_dir():
-        logger.warning(f"Folder {folder} already exists.")
+    if Path(main_folder).is_dir():
+        logger.warning(f"Folder {main_folder} already exists.")
         return False
 
     # create folder if not exist
-    Path(f"{folder}").mkdir()
-    logger.info(f"Folder projet: {folder} create successfully.")
+    Path(f"{main_folder}").mkdir()
+    logger.info(f"Folder projet: {main_folder} create successfully.")
+
+    # create project folders
+    folders = ['core', 'models', 'routers']
+    for folder in folders:
+        logger.info(f"Folder projet: {main_folder}/{folder} create successfully.")
+        Path(f"{main_folder}/{folder}").mkdir()
+
+    # create main.py
+    update_file_with_content(f"{main_folder}/main.py", "")
 
     # save project file in root project folder
     update_file_with_content(project_file, json.dumps(file_project, indent=4))
--- tests/test_create.py
+++ tests/test_create.py
@@ -1,9 +1,18 @@
 from apijet.create import create
 from apijet.utils.opfile import remove_project
+from pathlib import Path
 
 
 def test_create_projet():
     assert create('test_project', 5083, './') is True
+
+
+def test_project_folder_structure():
+    folders = ['core', 'models', 'routers']
+    for folder in folders:
+        assert Path(f'./test_project/{folder}').is_dir() is True
+
+    assert Path('./test_project/main.py').is_file() is True
 
 
 def test_folder_already_exist():
