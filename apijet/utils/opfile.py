700b38b82a9e218680aaefc9567285f0e5e714bf
Alfonso Farruggia <alfarruggia@gmail.com>
2022-09-08 05:42:27 +0100

    create folder and file project flake ok)

--- apijet/utils/opfile.py
+++ apijet/utils/opfile.py
@@ -1,9 +1,9 @@
-from asyncio.log import logger
 from loguru import logger
 from pathlib import Path
 import os
 
-def update_file_with_content(file_path:str, content:str):
+
+def update_file_with_content(file_path: str, content: str):
     try:
         f = open(file_path, "a")
         f.write(content)
@@ -12,12 +12,14 @@ def update_file_with_content(file_path:str, content:str):
     except Exception as e:
         logger.warning(f"Error during write file - {e}.")
 
-def check_write_permission(folder_path:str):
+
+def check_write_permission(folder_path: str):
     write_permission = os.access(folder_path, os.W_OK)
     logger.info(f"Folder {folder_path} write permission {write_permission}")
     return write_permission
 
-def remove_project(file_path:str):
+
+def remove_project(file_path: str):
     pth = Path(file_path)
     for child in pth.glob('*'):
         if child.is_file():
@@ -25,4 +27,4 @@ def remove_project(file_path:str):
         else:
             remove_project(child)
     pth.rmdir()
-    return Path(file_path).is_dir() == False
+    return Path(file_path).is_dir() is False
--- tests/test_create.py
+++ tests/test_create.py
@@ -1,9 +1,11 @@
 from apijet.create import create
 from apijet.utils.opfile import remove_project
 
+
 def test_create_projet():
-    assert create('test_project', 5083, './') == True
+    assert create('test_project', 5083, './') is True
+
 
 def test_folder_already_exist():
-    assert create('test_project', 5083, './') == False
-    assert remove_project('./test_project') == True
+    assert create('test_project', 5083, './') is False
+    assert remove_project('./test_project') is True
