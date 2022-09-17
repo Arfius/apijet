a92d72b484e2d188adc21c9d54f1492ef27547f1
Alfonso Farruggia <alfarruggia@gmail.com>
2022-09-17 09:29:07 +0100

    license + readme

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import find_packages, setup
 from apijet.version import __version__
 setup(
-    name="apiJet",
+    name="apijet",
     version=f"{__version__}",
     author="Alfonso Farruggia",
-    author_email="alfonso.farruggia@gmail.com",
-    description=("CLI tool to generate and deploy API"),
-    license="BSD",
-    keywords="api, cli",
-    url="git.url",
+    author_email="alfarruggia@gmail.com",
+    description=("A command line tool to deploy python Rest APIS in 20 secs"),
+    license="MIT",
+    keywords="api, mongodb, rest-api, cli-app, fastapi",
+    url="https://github.com/Arfius/apijet",
     packages=find_packages(),
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     entry_points={
         'console_scripts': ['apijet=apijet.main:main']
-    }
+    },
+    license_files=('LICENSE',)
 )
