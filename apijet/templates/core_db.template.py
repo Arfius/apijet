d4038a70f4b6ee0d628d07398416d45b9f8b37f9
Alfonso Farruggia <alfarruggia@gmail.com>
2022-09-18 09:40:00 +0100

    CRUD for database endpoint

--- apijet/templates/core_db.template.py
+++ apijet/templates/core_db.template.py
@@ -1,19 +1,33 @@
 from typing import List
 from {project_name}.models.message_db import MessageDB
 from {project_name}.models.{endpoint_name} import {endpoint_name}BaseDB, {endpoint_name}FullDB
-from {project_name}.repository.{endpoint_name} import get_{endpoint_name}, get_{endpoint_name}_by_id, save_{endpoint_name}
+from {project_name}.repository.{endpoint_name} import get_{endpoint_name}
+from {project_name}.repository.{endpoint_name} import save_{endpoint_name}
+from {project_name}.repository.{endpoint_name} import update_{endpoint_name}
+from {project_name}.repository.{endpoint_name} import get_all_{endpoint_name}
+from {project_name}.repository.{endpoint_name} import delete_{endpoint_name}
 
 
 class {endpoint_name}Core:
     @staticmethod
     def get_all() -> List[{endpoint_name}FullDB]:
-        return list(get_{endpoint_name}())
+        return list(get_all_{endpoint_name}())
+
+    @staticmethod
+    def get_one(pid: str) -> {endpoint_name}FullDB:
+        return get_{endpoint_name}(pid)
 
     @staticmethod
     def save_one(data: {endpoint_name}BaseDB) -> MessageDB:
-        id = save_{endpoint_name}(data.dict())
-        return MessageDB(id=str(id.inserted_id))
+        ret = save_{endpoint_name}(data.dict())
+        return MessageDB(id=str(ret.inserted_id))
     
     @staticmethod
-    def get_one(pid: str) -> {endpoint_name}FullDB:
-        return {endpoint_name}FullDB(**get_{endpoint_name}_by_id(pid))
+    def update_one(pid:str, data: {endpoint_name}FullDB) -> MessageDB:
+        ret = update_{endpoint_name}(pid, data.dict())
+        return MessageDB(modified_count=str(ret.modified_count))
+
+    @staticmethod
+    def delete_one(pid: str) -> MessageDB:
+        ret = delete_{endpoint_name}(pid)
+        return MessageDB(deleted_count=str(ret.deleted_count))
--- apijet/templates/router_db.template.py
+++ apijet/templates/router_db.template.py
@@ -1,7 +1,9 @@
 from typing import List
+from urllib import response
 from fastapi import APIRouter, HTTPException, Depends
 from {project_name}.models.{endpoint_name} import {endpoint_name}BaseDB, {endpoint_name}FullDB
 from {project_name}.core.{endpoint_name} import {endpoint_name}Core
+from {project_name}.models.message_db import MessageDB
 
 
 {endpoint_name}_router = APIRouter()
@@ -15,7 +17,7 @@ async def get():
         raise HTTPException(status_code=503, detail=str(e)) 
 
 
-@{endpoint_name}_router.post('/{endpoint_name}_save', tags=['{endpoint_name}'])
+@{endpoint_name}_router.post('/{endpoint_name}_save', tags=['{endpoint_name}'], response_model=MessageDB)
 async def get(data:{endpoint_name}BaseDB):
     try:
         return {endpoint_name}Core.save_one(data)
@@ -23,9 +25,24 @@ async def get(data:{endpoint_name}BaseDB):
         raise HTTPException(status_code=503, detail=str(e)) 
 
 
-@{endpoint_name}_router.get('/{endpoint_name}_get_one/{{pid}}', tags=['{endpoint_name}'])
+@{endpoint_name}_router.get('/{endpoint_name}_get_one/{{pid}}', tags=['{endpoint_name}'], response_model={endpoint_name}FullDB)
 async def get(pid: str):
     try:
         return {endpoint_name}Core.get_one(pid)
     except Exception as e:
         raise HTTPException(status_code=503, detail=str(e))
+
+@{endpoint_name}_router.patch('/{endpoint_name}_update/{{pid}}', tags=['{endpoint_name}'], response_model=MessageDB)
+async def update(pid: str, data: {endpoint_name}BaseDB):
+    try:
+        return {endpoint_name}Core.update_one(pid, data)
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=str(e)) 
+
+
+@{endpoint_name}_router.delete('/{endpoint_name}_delete', tags=['{endpoint_name}'], response_model=MessageDB)
+async def delete(pid: str):
+    try:
+        return {endpoint_name}Core.delete_one(pid)
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=str(e)) 
